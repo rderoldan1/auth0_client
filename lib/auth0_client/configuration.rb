febca5eb36edaf0c5406345be1dcba40192980c9
Ruben Espinosa <rderoldan1@gmail.com>
2014-12-19 11:02:01 -0500

    user agent

--- lib/auth0_client/configuration.rb
+++ lib/auth0_client/configuration.rb
@@ -21,7 +21,7 @@ module Auth0
       self.endpoint       = ENV['AUTH0_ENDPOINT']
       self.client_id      = ENV['AUTH0_CLIENT_ID']
       self.client_secret  = ENV['AUTH0_CLIENT_Secret']
-      self.user_agent     = DEFAULT_USER_AGENT
+      #self.user_agent     = DEFAULT_USER_AGENT
     end
   end
 end
