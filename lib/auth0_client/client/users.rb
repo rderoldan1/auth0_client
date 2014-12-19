c2ccef08d5e623680511cd58024ec16e2d01d00a
Mateo Vidal <jvidalba@gmail.com>
2014-12-19 15:25:42 -0500

    refactoring user methods

--- lib/auth0_client/client/users.rb
+++ lib/auth0_client/client/users.rb
@@ -2,15 +2,22 @@ class Auth0::Client
   module Users
 
     def users(options={})
-      token = authenticate["access_token"]
-      path = "/api/users?access_token=#{token}"
-      path.concat("&search=#{options[:criteria]}") if options[:criteria]
-      get(path)
+      get_auth_token do
+        path = "/api/users?access_token=#{@token}"
+        path.concat("&search=#{options[:criteria]}") if options[:criteria]
+        get(path)
+      end
     end
 
     def user(user_id, options={})
-      token = authenticate["access_token"]
-      get("/api/users/#{user_id}?access_token=#{token}")
+      get_auth_token do
+        get("/api/users/#{user_id}?access_token=#{@token}")
+      end
+    end
+
+    def get_auth_token
+      @token = authenticate["access_token"]
+      yield
     end
 
   end
