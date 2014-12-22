5819dd22ef7eba1ee52b35cdab2c7b780bbbc078
Mateo Vidal <jvidalba@gmail.com>
2014-12-22 16:36:04 -0500

    testing GET requests

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -2,7 +2,41 @@ require 'spec_helper'
 
 describe 'User API Endpoint' do
 
-  it 'should do something' do
-    expect(true).to be true
+	before(:all) do
+    @client = Auth0.client  
+    @user = @client.create_user(Faker::Internet.email, Faker::Internet.password)
   end
+
+  after(:all) do
+    @client.delete_user(@user["user_id"])
+  end
+
+  describe "GET methods" do
+    it 'should get all users' do
+      users = @client.users
+      expect(users.first["email"]).to eq(@user["email"])
+      expect(users.first["user_id"]).to eq(@user["user_id"])
+    end
+
+    it 'should get all users with criteria' do
+      users = @client.users(criteria: "loginsCount:10")
+      expect(users).to be_empty
+    end
+
+    it 'should get a user by id' do
+      user = @client.user(@user["user_id"])
+      expect(user["email"]).to eq(@user["email"])
+      expect(user["user_id"]).to eq(@user["user_id"])
+    end
+  end
+
+  describe "POST methods" do
+  end
+
+  describe "PUT methods" do
+  end
+
+  describe "DELETE methods" do
+  end
+	
 end
